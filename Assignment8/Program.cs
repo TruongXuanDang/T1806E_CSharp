1f8d3511ab14bc2641511d17a4b8c8fec7d623b0
Unknown <truonghusk17@gmail.com>
2019-05-21 19:37:49 +0700

    Assignment8

---
+++ Assignment8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericArrayList<string, int> myGenericArrayList = new MyGenericArrayList<string, int>();
+            myGenericArrayList.key = "abc";
+            myGenericArrayList.value = 10;
+            Console.WriteLine(myGenericArrayList.key);
+            Console.WriteLine(myGenericArrayList.value);
+            Console.ReadKey();
+        }
+    }
+
+    public class MyGenericArrayList<K,V>
+    {
+        public K key;
+        public V value;
+    }
+}
