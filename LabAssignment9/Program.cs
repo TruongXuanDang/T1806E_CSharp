558d5a1ffd357e642fffd8042f22cc3b1b004022
Unknown <truonghusk17@gmail.com>
2019-05-25 09:24:57 +0700

    LabAssignment9

---
+++ LabAssignment9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LabAssignment9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunOnThread("Remain time");
+            Thread t = new Thread(RunOnThread);
+            Console.ReadKey();
+       
+
+        }
+
+        public static void RunOnThread(Object a)
+        {
+            String s = (String)a;
+            for (int i = 10; i >= 0; i--)
+            {
+                Console.WriteLine(s+"--"+i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Congratulation!!!");
+            
+        }
+    }
+}
